3b11c8b69f5c38e4e18272132e25fe2d5540b53b
Twind2 <sunzihang556@gmail.com>
2026-06-08 21:26:03 +0800

    Initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+(function(){
+  "use strict";
+
+  window.LIFEIF_BUILTIN_API_CONFIG = {
+    mockMode: false,
+    streamMode: true,
+    jsonMode: true,
+    apiUrl: "",
+    apiModel: "",
+    thinkingType: "disabled",
+    reasoningEffort: "high",
+
+    
+    apiKey: ""
+  };
+})();
